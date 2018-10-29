4ed56b82049a0c01cd3e4be85d2aa84ce6879bfb
DanFein <danfeinstat@gmail.com>
2018-10-29 12:52:40 -0700

    updated server

--- server.js
+++ server.js
@@ -14,6 +14,9 @@ app.use((req, res, next) => {
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use(express.static(path.join(__dirname, "client/build")));
+app.use(routes);
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 
