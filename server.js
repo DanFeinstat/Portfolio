433aa41114a06121ad770e5164933ffb1df8702b
DanFein <danfeinstat@gmail.com>
2018-10-29 13:54:14 -0700

    updated server

--- server.js
+++ server.js
@@ -1,22 +1,26 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3002;
 
+//allow cross origin access
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
   res.setHeader("Access-Control-Allow-Headers", "Content-type,Authorization");
   next();
 });
 
+// Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 if (process.env.NODE_ENV === "production") {
+  // Exprees will serve up production assets
   app.use(express.static("client/build"));
 
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require("path");
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
